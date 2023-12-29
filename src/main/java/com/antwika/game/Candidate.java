9b7a559cb4ceaa4c4c3e45055b194021805c73c1
ixuz <ixuz07@gmail.com>
2023-12-29 22:03:34 +0100

    wip

--- src/main/java/com/antwika/game/Candidate.java
+++ src/main/java/com/antwika/game/Candidate.java
@@ -10,17 +10,4 @@ import java.util.Objects;
 public class Candidate {
     private Seat seat;
     private int amount;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Candidate candidate = (Candidate) o;
-        return amount == candidate.amount && Objects.equals(seat, candidate.seat);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(seat, amount);
-    }
 }
