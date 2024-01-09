73fd8b869292394363e7b38487a4a79a5ea132d5
ixuz <ixuz07@gmail.com>
2024-01-09 22:55:13 +0100

    wip

--- src/main/java/com/antwika/game/game/Game.java
+++ src/main/java/com/antwika/game/game/Game.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @Getter
-public class Game extends ActionHandler {
+public class Game extends EventHandler {
     private static final Logger logger = LoggerFactory.getLogger(Game.class);
 
     private final GameData gameData;
@@ -21,8 +21,11 @@ public class Game extends ActionHandler {
 
     boolean shouldStopAfterHand = false;
 
+    private final IHandler handler;
+
     public Game(long maxHandCount, long eventPollTimeoutMillis) {
-        super(new AggregateHandler(List.of(
+        super("Game", eventPollTimeoutMillis);
+        this.handler = new AggregateHandler(List.of(
                 new ShowdownRequestHandler(),
                 new HandBeginRequestHandler(),
                 new PlayerJoinRequestHandler(),
@@ -32,7 +35,7 @@ public class Game extends ActionHandler {
                 new OrbitActionRequestHandler(),
                 new OrbitActionResponseHandler(),
                 new DealCommunityCardsRequestHandler()
-        )), "Game", eventPollTimeoutMillis);
+        ));
         this.maxHandCount = maxHandCount;
         gameData = GameDataFactory.createGameData(1L, "Lacuna I", 6, 5, 10);
     }
@@ -40,6 +43,31 @@ public class Game extends ActionHandler {
     @Override
     public synchronized void stopThread() {
         shouldStopAfterHand = true;
+    }
+
+    @Override
+    public synchronized void handle(IEvent event) {
+        try {
+            boolean handled = false;
+            if (handler.canHandle(event)) {
+                logger.debug("Handle: {}", event);
+
+                final List<IEvent> additionalEvents = handler.handle(event);
+                if (additionalEvents != null) {
+                    for (IEvent additionalEvent : additionalEvents) {
+                        offer(additionalEvent);
+                    }
+                }
+
+                handled = true;
+            }
+
+            if (!handled) {
+                logger.warn("No action handler could handle: {}", event);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -60,5 +88,10 @@ public class Game extends ActionHandler {
         } catch (InterruptedException e) {
             logger.info("Interrupted", e);
         }
+    }
+
+    @Override
+    protected void preEventHandle() {
+
     }
 }
